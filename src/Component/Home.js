8e7b9cb543610c1d7943b35fe738324e228420a8
prakarsh shrivastava <prakar.vastava@gmail.com>
2021-04-25 17:18:30 +0530

    First commit

---
+++ src/Component/Home.js
@@ -0,0 +1,153 @@
+import React, {useState} from 'react';
+import { Button } from 'primereact/button';
+import Modal from './Modal';
+import Table from './Table';
+
+const Home = () => {
+
+    const defaultPendingData = [{
+        id: 1,
+        summary: "Buy groceries",
+        desciption: "Buy groceries from market, do not forget Coriander ",
+        dueDate: "2021-04-26",
+        priority: "High",
+        currentState: "open",
+        createdOn: "2021-04-24"
+    },
+    {
+        id: 2,
+        summary: "Complete the designs",
+        desciption: "Finish all the pending designs",
+        dueDate: "2021-04-30",
+        priority: "Medium",
+        currentState: "open",
+        createdOn: "2021-04-24"
+    },
+    {
+        id: 4,
+        summary: "Do research on the assignment",
+        desciption: "prepare research data for the assignment",
+        dueDate: "2021-04-27",
+        priority: "High",
+        currentState: "open",
+        createdOn: "2021-04-24"
+    }];
+    const defaultCompletedData = [ {
+        id: 3,
+        summary: "Sleep well",
+        desciption: "Sleep well for atleast 7 hours",
+        dueDate: "2021-04-26",
+        priority: "None",
+        currentState: "Done",
+        createdOn: "2021-04-24"
+    }];
+    const [showPopup, setShowPopup] = useState(false);
+    const [dialogFor, setDialogFor] = useState("");
+    const [pendingTaskDetails, setPendingTaskDetails] = useState(defaultPendingData);
+    const [completedTaskDetails, setCompletedTaskDetails] = useState(defaultCompletedData);
+    const [id, setId] = useState(5);
+
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    }
+
+    const onClickAdd = () => {
+        setDialogFor("Add");
+        togglePopup();
+    }
+
+    const onSave = (taskDetail, type) => {
+        let tempPendingTaskDetails = [...pendingTaskDetails];
+        let tempCompletedTaskDetails = [...completedTaskDetails];
+        if (type === "Add") {
+            setId(taskDetail.id + 1);
+            tempPendingTaskDetails.unshift(taskDetail);
+            setPendingTaskDetails(tempPendingTaskDetails);
+        }else if(type === "Edit") {
+            if(taskDetail.currentState === "Done"){
+                let completedTask = []
+                tempCompletedTaskDetails.forEach(task => {
+                   if(task.id === taskDetail.id){
+                       completedTask.push(taskDetail)
+                   }else{
+                    completedTask.push(task)
+                   }
+                })
+                setCompletedTaskDetails(completedTask);
+            }else{
+                let pendingTask = []
+                tempPendingTaskDetails.forEach(task => {
+                    if(task.id === taskDetail.id){
+                        pendingTask.push(taskDetail)
+                    }else{
+                        pendingTask.push(task)
+                    }
+                 })
+                 setPendingTaskDetails(pendingTask);
+            }
+        }
+    }
+
+    
+    const markAsDone = (id) => {
+        const tempPendingTaskDetails = [...pendingTaskDetails]
+        const tempCompletedTaskDetails = [...completedTaskDetails]
+        tempPendingTaskDetails.forEach(task => {
+            if(task.id === id){
+                task['currentState'] = "Done";
+                tempCompletedTaskDetails.push(task);
+                setCompletedTaskDetails(tempCompletedTaskDetails);
+            }
+        })
+        setPendingTaskDetails(tempPendingTaskDetails.filter((task)=>task.id!==id));
+    }
+
+    const reOpen = (id) =>{
+        const tempPendingTaskDetails = [...pendingTaskDetails]
+        const tempCompletedTaskDetails = [...completedTaskDetails]
+        tempCompletedTaskDetails.forEach(task => {
+            if(task.id === id){
+                task['currentState'] = "Open";
+                tempPendingTaskDetails.push(task);
+                setPendingTaskDetails(tempPendingTaskDetails);
+            }
+        })
+        setCompletedTaskDetails(tempCompletedTaskDetails.filter((task)=>task.id!==id));
+    }
+
+    const deleteTask = (id,status)=>{
+        if(status === "Done"){
+            const tempCompletedTaskDetails = [...completedTaskDetails];
+            setCompletedTaskDetails(tempCompletedTaskDetails.filter((task)=>task.id!==id));
+        }else{
+            const tempPendingTaskDetails = [...pendingTaskDetails];
+            setPendingTaskDetails(tempPendingTaskDetails.filter((task)=>task.id!==id));
+        }
+    }
+    return (
+        <React.Fragment>
+            <Button icon="pi pi-plus" title="Add Task" onClick={onClickAdd} className="p-button-rounded addButton " style={{position: 'absolute', marginTop: '0.5%', marginLeft: '82%'}} />
+            <h3>ToDo App</h3>
+            
+            <br />
+
+            <Table pendingTaskDetails={pendingTaskDetails}
+                completedTaskDetails={completedTaskDetails}
+                markAsDone={markAsDone}
+                reOpen={reOpen} 
+                deleteTask={deleteTask}
+                onSave = {onSave}/>
+
+            {showPopup &&
+                <Modal visible={showPopup}
+                    toggle={togglePopup}
+                    modalFor={dialogFor}
+                    id={id}
+                    onSave={onSave} />
+            }
+
+        </React.Fragment>
+    )
+
+}
+export default Home;
---
+++ src/Component/Table.js
@@ -0,0 +1,194 @@
+import React, {useState, useRef } from 'react';
+import { Button } from 'primereact/button';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { TabView, TabPanel } from 'primereact/tabview';
+import { Row, Col } from 'reactstrap';
+import Modal from './Modal';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import { Dialog } from 'primereact/dialog';
+import '../style/main.css'
+
+const Table = (props) => {
+
+    let datatableRef = useRef(null);
+
+    const groupByList = [
+        { label: 'None', value: 'none' },
+        { label: 'Created On', value: 'createdOn' },
+        { label: 'Pending On', value: 'dueDate' },
+        { label: 'Priority', value: 'priority' },
+        ];
+    const [groupBy, setGroupBy] = useState("none");
+    const [search, setSearch] = useState("");
+    const [dialog, setDialog] = useState("");  //dailogfor
+    const [deleteData, setDeleteData] = useState("");
+    const [confirmDelete, setConfirmDelete] = useState(false); //showDelete
+    const [showModal, setShowModal] = useState(false); // show popup
+    const [selectedData, setSelectedData] = useState(''); //select data
+    const [index, setIndex] = useState(0); //activeindex
+
+    const onGroupSelect = (e) => {
+        setGroupBy(e.target.value);
+    }
+
+
+    const footerTemplate = (data) => {
+        return (
+            <React.Fragment></React.Fragment>
+        );
+    }
+
+    const commonDataTable = (data) => {
+        return (
+            groupBy !== "none" ?
+                <DataTable value={data} className="p-datatable-gridlines"
+                    rowClassName={markAsDone}
+                    onRowClick={e => onRowClick(e.data)}
+                    rowGroupMode="subheader" groupField={groupBy}
+                    sortMode="single" sortField={groupBy} sortOrder={1}
+                    rowGroupHeaderTemplate={headerTemplate} rowGroupFooterTemplate={footerTemplate}
+                    ref={(el)=> datatableRef = el}
+                >
+                    <Column field="summary" header="Summary" />
+                    <Column field="priority" header="Priority" />
+                    <Column field="createdOn" header="Created On" />
+                    <Column field="dueDate" header="Due By" />
+                    <Column header="Action" body={action} />
+                </DataTable> :
+                <DataTable value={data} className="p-datatable-gridlines"
+                    rowClassName={markAsDone}
+                    onRowClick={e => onRowClick(e.data)}
+                    ref={(el)=> datatableRef = el}
+                >
+                    <Column field="summary" header="Summary" sortable />
+                    <Column field="priority" header="Priority" sortable />
+                    <Column field="createdOn" header="Created On" sortable />
+                    <Column field="dueDate" header="Due By" sortable />
+                    <Column header="Action" body={action} />
+                </DataTable>)
+    }
+
+    const headerTemplate = (data) => {
+        return <span className="subHeaderClass text-uppercase font-weight-bold">{data[groupBy]}</span>
+    }
+
+
+    const onRowClick = (e) => {
+        setSelectedData(e);
+        setDialog("View");
+        togglePopup();
+    }
+
+    const markAsDone = (e) => {
+        if (index === 0) {
+            return { 'taskDone': e.currentState === "Done" }
+        }
+    }
+
+    const action = (rowData) => {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'space-between', textDecoration: 'none !important' }}>
+                <Button icon="pi pi-pencil" title="Edit Task" onClick={e => { e.stopPropagation(); onClickEdit(rowData) }} className=" p-button-info" />
+                {rowData.currentState === "Done" ?
+                    <Button label="ReOpen" onClick={e => { e.stopPropagation(); props.reOpen(rowData.id) }} className=" p-button-primary " />
+                    : <Button label="Done" onClick={e => { e.stopPropagation(); props.markAsDone(rowData.id) }} className=" p-button-success " />
+                }
+                <Button icon="pi pi-ban" title="Delete Task" onClick={e => { e.stopPropagation(); onClickDelete(rowData) }} className="p-button-danger" />
+            </div>
+        )
+    }
+
+    const onClickDelete = (rowData) => {
+        setDeleteData(rowData);
+        toggleDelete();
+    }
+
+    const toggleDelete = () => {
+        setConfirmDelete(!confirmDelete);
+    }
+
+    const onClickEdit = (rowData) => {
+        setSelectedData(rowData);
+        setDialog("Edit");
+        togglePopup();
+    }
+
+    const togglePopup = () => {
+        setShowModal(!showModal);
+    }
+
+    const onSave = (taskDetail, type) => {
+        props.onSave(taskDetail, type);
+    }
+
+    const onSearchChange = (e) => {
+        setSearch(e.target.value);
+        datatableRef.filter(e.target.value, 'summary', 'startsWith');
+    }
+
+    const footer = (
+                <span>
+                    <Button label="No" onClick={toggleDelete} className="p-button-secondary" />
+                    <Button label="Yes" onClick={e=>{props.deleteTask(deleteData.id,deleteData.currentState);toggleDelete()}} className="p-button-danger" />
+
+                </span>
+            )
+        
+    return (
+        <div>
+            <Row>
+                <label className="control-label col-md-2" style={{ paddingLeft: "1.5%" }}>Group By</label>
+                <label className="control-label col-md-10" style={{ paddingLeft: "2%" }}>Search</label>
+            </Row>
+            <Row>
+                <Col md="2" sm="2">
+                    <Dropdown value={groupBy}
+                        options={groupByList}
+                        onChange={e => onGroupSelect(e)}
+                        style={{ width: "100%" }}
+                    />
+                </Col>
+                <Col md="8" sm="8">
+                    <InputText value={search}
+                        onChange={e => onSearchChange(e)}
+                        style={{ width: "100%" }}
+                        placeholder="Search Tasks" />
+                </Col>
+            </Row>
+            <br />
+            <Row>
+                <TabView header="ToDo List" activeIndex={index} onTabChange={e => setIndex(e.index)} >
+                    <TabPanel header="All">
+                        {commonDataTable(props.pendingTaskDetails.concat(props.completedTaskDetails))}
+                    </TabPanel>
+                    <TabPanel header="Pending">
+                        {commonDataTable(props.pendingTaskDetails)}
+                    </TabPanel>
+                    <TabPanel header="Completed">
+                        {commonDataTable(props.completedTaskDetails)}
+                    </TabPanel>
+                </TabView>
+            </Row>
+            {confirmDelete &&
+                <Dialog header="Confirm" style={{ width: '40%' }} visible={confirmDelete}
+                    onHide={toggleDelete} footer={footer}>
+                        <div style={{marginBottom:'1rem'}}><b>Summary:</b> {deleteData.summary}</div>
+                            
+                        <div>Do you want to delete this task?</div>
+
+                </Dialog>
+            }
+            {showModal &&
+                <Modal visible={showModal}
+                    toggle={togglePopup}
+                    modalFor={dialog}
+                    selectedData={selectedData}
+                    onSave={onSave}
+                />
+            }
+        </div>
+    )
+}
+export default Table;
